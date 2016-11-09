5133a6e3831e6bded7fa674e6a0fee96ac4f339c
Pieter Goetschalckx <3.14.e.ter@gmail.com>
2016-11-09 15:57:53 +0100

    Fix Wikipedia download

--- download.sh
+++ download.sh
@@ -4,4 +4,4 @@ curl "http://www.gutenberg.org/cache/epub/4078/pg4078.txt" | tr "[:space:]" " " 
 
 curl "ftp://ftp.ncbi.nlm.nih.gov/genomes/Homo_sapiens/CHR_01/hs_ref_GRCh38.p7_chr1.fa.gz" | gunzip | tail -n +2 | tr -d '\n' > dna.txt
 
-curl -d "&pages=$(cat wikipages.txt)" "http://en.wikipedia.org/w/index.php?title=Special:Export" | tr -cd "[:alnum:][:space:]" | tr "[:upper:]" "[:lower:]" | tr "[:space:]" " " | tr -s " " > wikipedia.txt
+curl -d "&pages=$(cat wikipages.txt)" "https://en.wikipedia.org/w/index.php?title=Special:Export" | tr -cd "[:alnum:][:space:]" | tr "[:upper:]" "[:lower:]" | tr "[:space:]" " " | tr -s " " > wikipedia.txt
